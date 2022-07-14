3e8ff5a32b1c2f638d1c33eac1235c76ef19097e
unknown <prossdube@gmail.com>
2022-07-14 16:45:15 +0200

    first commit

---
+++ app.ts
@@ -0,0 +1,26 @@
+const inputName = document.querySelector('#name') as HTMLInputElement
+const inputAge = document.querySelector('#age') as HTMLInputElement
+const inputForm = document.querySelector('form')!;
+const greeting = document.querySelector('.greeting') as HTMLDivElement;
+
+interface PersonInterface{
+    name: string;
+    age: number;
+}
+
+class Person implements PersonInterface {
+    constructor(public name: string, public age: number) {}
+
+    greet() {
+        return `Hi, my name is ${this.name} and i am ${this.age}.`
+    }
+}
+inputForm.addEventListener('submit', (e) => {
+e.preventDefault();
+
+const person = new Person(inputName.value, inputAge.valueAsNumber)
+
+greeting.innerText = person.greet();
+
+inputForm.reset();
+});
---
+++ code/app.js
@@ -0,0 +1,4 @@
+"use strict";
+let aNum = 5;
+
+
